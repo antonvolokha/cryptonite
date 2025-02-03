15bdc12a97ee23d7a1f31fd28cd568f9dd78fbd5
Anton Volokha <antonvolokha@gmail.com>
2025-02-04 00:37:00 +0200

    Add test files

--- tests/simple.enc.sh
+++ tests/simple.enc.sh
@@ -1,2 +1,6 @@
-go run ../cmd/main.go -encrypt -input ./data/very_secret_doc.pdf -output container.enc -password mysecretpass
-go run cmd/main.go -decrypt -input container.enc -output /path/to/extract -password mysecretpass
+PASSWORD="mysecretpass"
+CONTAINER_NAME="container.enc"
+FILE_NAME="very_secret_doc.pdf"
+
+go run ../cmd/main.go -encrypt -input ./data/$FILE_NAME -output $CONTAINER_NAME -password $PASSWORD
+go run ../cmd/main.go -decrypt -input container.enc -output ./results/$FILE_NAME -password $PASSWORD
